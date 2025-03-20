e0979f0c0e9b233aaa6ea2d4d17ed1be18bc37d9
cece554 <cece554@gmail.com>
2025-03-20 18:52:31 -0500

    Added Letter Pool and added psuedo code for the random draw function

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,30 @@
 from random import randint
-
+# Letter pool that represents each letter and how many times it shows in the pool
 def draw_letters():
-    pass
+    letter_pool = (
+        ["A"] * 9 + ["B"] * 2 + ["C"] * 2 + ["D"] * 4 + 
+        ["E"] * 12 + ["F"] * 2 + ["G"] * 3 + ["H"] * 2 +
+        ["I"] * 9 + ["J"] * 1 + ["K"] * 1 + ["L"] * 4 +
+        ["M"] * 2 + ["N"] * 6 + ["O"] * 8 + ["P"] * 2 +
+        ["Q"] * 1 + ["R"] * 6 + ["S"] * 4 + ["T"] * 6 +
+        ["V"] * 4 + ["W"] * 2 + ["X"] * 1 + ["Y"] * 2 + ["Z"] * 1
+    )
+
+# # Add letters to list
+# loop each (letter, quantity) in letter_distribution: 
+#         add letter to letter_pool quantity times   
+
+#     # Keep track of drawn letters 
+#     drawn_letters = []  
+
+#     # 10x it an randomize append to list
+#     loop 10 times:  
+#         random.function = random number between 0 and len(letter_pool) - 1  
+#         letter = letter_pool[random.function]  
+#         add letter to drawn_letters  
+
+#     # return letters
+#     return drawn_letters  
 
 def uses_available_letters(word, letter_bank):
     pass
@@ -11,3 +34,39 @@ def score_word(word):
 
 def get_highest_word_score(word_list):
     pass
+
+
+
+
+# Your first task is to build a hand of 10 letters for the user. To do so, implement the function `draw_letters` in `game.py`. This method should have the following properties:
+
+#   - No parameters
+#   - Returns an array of ten strings
+#   - Each string should contain exactly one letter
+#   - These represent the hand of letters that the player has drawn
+#   - The letters should be randomly drawn from a pool of letters
+#   - This letter pool should reflect the distribution of letters as described in the table below
+#   - There are only 2 available `C` letters, so `draw_letters` cannot ever return more than 2 `C`s
+#   - Since there are 12 `E`s but only 1 `Z`, it should be 12 times as likely for the user to draw an `E` as a `Z`
+# - Invoking this function should **not** change the pool of letters
+#   - Imagine that the user returns their hand to the pool before drawing new letters
+
+# #### Distribution of Letters
+
+# | Letter : Qty. | Letter : Qty. |
+# |:------:|:-----:|
+# | A : 9  | N : 6 |
+# | B : 2  | O : 8 |
+# | C : 2  | P : 2 |
+# | D : 4  | Q : 1 |
+# | E : 12 | R : 6 |
+# | F : 2  | S : 4 |
+# | G : 3  | T : 6 |
+# | H : 2  | U : 4 |
+# | I : 9  | V : 2 |
+# | J : 1  | W : 2 |
+# | K : 1  | X : 1 |
+# | L : 4  | Y : 2 |
+# | M : 2  | Z : 1 |
+
+# **Note:** Making sure that the drawn letters match the rules of the letter pool can be straightforward or very difficult, depending on how you build the data structure for the letter pool. It is worth spending some time to think carefully about this.
